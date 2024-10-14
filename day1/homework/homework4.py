2271baf039f62a3f22b6844f7f8bb948b6cde06e
saba871 <sabatabatadze40@gmail.com>
2024-10-14 16:35:54 +0400

    ninth commit

--- day1/homework/homework4.py
+++ day1/homework/homework4.py
@@ -21,3 +21,36 @@ father = age2 = 44
 dog1 = age3 = 5
 dog2 = age4 = 3
 print ( age1 + age2 + age3 + age4 )
+
+
+
+
+print(3 + 5)    
+print(10 + 15)
+print(1 + 7)     
+print(2 + 8)   
+print(4 + 5)
+
+print(10 - 5)  
+print(40 - 30)  
+print(7 - 0)   
+print(5 - 3)   
+print(8 - 2)
+
+print(3 * 5)   
+print(7 * 2)    
+print(4 * 9)   
+print(2 * 2)  
+print(1 * 2) 
+
+print(10 / 2)  
+print(9 / 3)    
+print(15 / 3)  
+print(8 / 2)   
+print(7 / 7)
+
+
+
+
+# droebiti mexsiereba aris sistema romelic inaxavs monacemebs mokle drois ganmavlobashi
+# xolo misi daxmarebit kompiuteri bevrad swrapad mushaobs.
