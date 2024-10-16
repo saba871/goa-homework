28ca25b9de7b896111fb1da777f1e73db54bafd0
saba871 <sabatabatadze40@gmail.com>
2024-10-16 19:43:06 +0400

    eleventh commit

---
+++ day1/homework/homework6.py
@@ -0,0 +1,66 @@
+
+# chosen_number =input("choose number from one to seven: ")
+# if chosen_number =="1":
+#     print("mondey")
+# elif chosen_number == "2":
+#     print("thusdey")
+# elif chosen_number == "3":
+#     print("wendsday")
+# elif chosen_number == "4":
+#     print("Thursday")
+# elif chosen_number == "5":
+#     print("friday")
+# elif chosen_number == "6":
+#     print("seturday")  
+# elif chosen_number == "7":
+#     print("sunday")
+# else:
+#     print("i said one to seven ")
+
+
+
+
+# # ეკითხება მომხმარებელს ორივე რიცხვის მნიშვნელობას რათა შეადაროს ისინი
+# num1 =int(input("what's the first number? "))
+# num2 =int(input("what's the second number? "))
+
+# if num1 < num2:
+#     print(num1 > num2)
+# elif num1 > num2:
+#     print(num1 > num2) 
+# elif num1 <= num2:
+#     print(num1 <= num2)
+
+
+
+
+
+# age = int(input("what's your age? "))
+# if age < 18:
+#     print("you are kid ")
+# elif age >= 18:
+#     print("you are an adult ")
+
+
+
+
+
+# 6)მომხარებელს შემოატანინეთ 4 რიცხვი. შეინახეთ ისინი ცვლადებში და დაბეჭდეთ მათი საშუალო არითმეტიკული
+
+
+
+
+
+
+
+
+# 3.როგორ დავბეჭდოთ მათი შაშუალო არითმეტიკული
+# ა.
+
+
+num1 =int(input("what's the first number? "))
+num2 =int(input("what's the second number? "))
+num3 =int(input("what's the third number? "))
+num4 =int(input("what's the forth number? "))
+print("mati sashualoa"((num1 + num2 + num3 + num4) / 3 )) 
+
