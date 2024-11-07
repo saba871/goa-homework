281c79836712007ae0578745e1c5ab14ad15a489
saba871 <sabatabatadze40@gmail.com>
2024-11-07 22:44:13 +0400

    second commit

--- day1/homework/homework17.py
+++ day1/homework/homework17.py
@@ -5,7 +5,7 @@ for string in strings:
     # 3) შექმენით რიცხვების სია, while loop-ის გამოყენებით გაიგეთ ამ სიაში ყველა ლუწი რიცხვის ჯამი და დაპრინტეთ
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 sum = 0
-index = 0 
+index = 0
 while index < len(numbers):
     if numbers[index] % 2 == 0:
         sum = sum + numbers[index]
@@ -27,21 +27,22 @@ print(no_duplicants)
 symbols = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(symbols[2:4])
     # 8) შექმენით რიცხვების სია, შემდგარი 10 ელემენტისგან. slicing-ის გამოყენებით დაბეჭდეთ სიის ყოველი მეორე ელემენტი
-numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(numbers[0:10:2])
     # 9) შექმენით ცვლადი, სადაც შეინახავთ string-ს. დაბეჭდეთ სთრინგის ბოლო სამი სიმბოლო(გამოიყენეთ slicing და მინუსიანი ინდექსები)
 name = "cars"
 print(name[1:5])
     # 10) შექმენით რიცხვების სია, დაბეჭდეთ სია თავიდან ბოლომდე slicing-ის გამოყენებით 
-numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(numbers[0:10])
 #     # 6) შექმენით პროგრამა, რომელიც მომხარებელს შემოატანინებს რიცხვს და დაბეჭდავს 1-დან შემოტანილ რიცხვამდე ყველა მარტივ რიცხვს
-num = int(input("enter any number: "))
-prime = []
-i = 0
-for i in range (0,num):
-    i += 1
-    if num < 100:
-        num +=1
-        prime.append(i)
-    print(prime)
+number = int(input("Please enter a number: "))
+result = []
+for num in range(1, number):
+    dividers = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            dividers.append(i)
+    if len(dividers) == 2:
+        result.append(num)
+print(result)
