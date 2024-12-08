531708b4d3bf5f74ae5fd01f164e2a2fb53cc725
saba871 <sabatabatadze40@gmail.com>
2024-12-08 14:57:18 +0400

    commit

--- day 34/classwork/classwork34.py
+++ day 34/classwork/classwork34.py
@@ -1,20 +1,11 @@
 
 
 
-# def capitals(word):
+def capitals(word):
     
-#     result = []
-#     for letter in range(len(word)):
-#         if word[letter].isupper():
-#             result.append(letter)
-#     return result
-# print(capitals("SaBA"))
-
-
-numbers = [1,2,3,4,5,6,7]
-
-odds =[]
-for number in numbers:
-    if number % 2 == 1:
-        odds.append(number)
-print(odds)
+    result = []
+    for letter in range(len(word)):
+        if word[letter].isupper():
+            result.append(letter)
+    return result
+print(capitals("SaBA"))
