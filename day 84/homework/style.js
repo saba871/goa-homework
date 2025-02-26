050cf2a9600622ea8f073139eb7c3eb7a93c5e9a
saba871 <sabatabatadze40@gmail.com>
2025-02-26 17:30:28 +0400

    commit

--- day 84/homework/style.js
+++ day 84/homework/style.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+const p = document.getElementById("count");
+const btn = document.getElementById("btn");
+
+let count = 0;
+
+function increment() {
+  count++;
+  p.textContent = `Count: ${count}`;
+}
+
+btn.onclick = increment;
